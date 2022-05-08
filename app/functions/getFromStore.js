092d9b18d6d8e408ced063ecde8b2d88502d2cd4
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-05-08 11:40:50 +0200

    getFrom Store works

--- app/functions/getFromStore.js
+++ app/functions/getFromStore.js
@@ -2,7 +2,6 @@ import * as SecureStore from "expo-secure-store";
 
 const getFromStore = async (key) => {
     try {
-        console.log(key)
         let obj = await SecureStore.getItemAsync(key);
         return obj;
     } catch (e) {
