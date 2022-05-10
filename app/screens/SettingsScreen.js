f18ed4ec433ed09ed9478770da012fcbfc8cacb7
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-05-10 10:52:55 +0200

    Settings zo goed als af

--- app/screens/SettingsScreen.js
+++ app/screens/SettingsScreen.js
@@ -7,47 +7,60 @@ import refreshToken from "../functions/refreshToken";
 import getAccessToken from "../functions/getAccessToken";
 import axios from "axios";
 import backendURL from "../backendURL";
+import removeFirstAndLast from "../functions/removeFirstAndLast";
 
 
 function SettingsScreen() {
     const [hasLoaded, setHasloaded] = useState(false);
     const [ownId, setOwnId] = useState('');
-    const [user, setUser] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [roles, setRoles] = useState([]);
+    const [favoriteSubjects, setFavoriteSubjects] = useState([]);
+    const [preferredSubjects, setPrefferedSubjects] = useState([]);
+    const [targetAudience, setTargetAudience] = useState([]);
 
     React.useEffect(()=> {
         const constructor = async () => {
             await refreshToken();
             let token = await getAccessToken();
             let id = await getFromStore("ownId");
-            setOwnId(id);
+            id = removeFirstAndLast(id)
 
             const axios = require('axios');
-
             const config = {
                 method: 'get',
-                url: backendURL + '/userManagement/users/' + ownId,
+                url: backendURL + '/userManagement/users/' + id,
                 headers: {
-                    'Authorization': 'Bearer ' + JSON.stringify(token)
+                    'Authorization': 'Bearer ' + JSON.parse(token)
                 }
             };
 
             axios(config)
                 .then(function (response) {
                     console.log(JSON.stringify(response.data));
-                    setUser(response.data)
+                    setLastName(response.data.lastName);
+                    setFirstName(response.data.firstName);
+                    setEmail(response.data.email);
+                    setPhone(response.data.telNr)
+                    setRoles(response.data.roles)
+                    setFavoriteSubjects(response.data.favouriteSubjects)
+                    setRoles(response.data.roles)
+                    setPrefferedSubjects(response.data.preferredSubjects)
+                    setTargetAudience(response.data.targetAudience)
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
 
-
-            setHasloaded(true);
+            //setHasloaded(true);
         }
         constructor();
     },[])
 
-    if(!hasLoaded) return null;
-
+    // if(!hasLoaded) return null;
     return(
         <View style={styleLoginLogout.basicContainer}>
             <Image
@@ -57,7 +70,7 @@ function SettingsScreen() {
 
             <View style={styleLoginLogout.infoLine}>
                 <Text style={styleLoginLogout.tag}>{"\t\t"} Name:</Text>
-                <Text style={styleLoginLogout.prop}>{user.lastName}</Text>
+                <Text style={styleLoginLogout.prop}>{lastName}</Text>
             </View>
 
             <View style = {styleLoginLogout.viewLine}/>
@@ -65,35 +78,43 @@ function SettingsScreen() {
 
             <View style={styleLoginLogout.infoLine}>
                 <Text style={styleLoginLogout.tag}>{"\t\t"} Firstname: </Text>
-                <Text style={styleLoginLogout.prop}>{user.firstName} </Text>
+                <Text style={styleLoginLogout.prop}>{firstName} </Text>
             </View>
 
             <View style = {styleLoginLogout.viewLine}/>
 
             <View style={styleLoginLogout.infoLine}>
                 <Text style={styleLoginLogout.tag}>{"\t\t"} Email: </Text>
-                <Text style={styleLoginLogout.prop}>{user.email}</Text>
+                <Text style={styleLoginLogout.prop}>{email}</Text>
             </View>
 
             <View style = {styleLoginLogout.viewLine}/>
 
             <View style={styleLoginLogout.infoLine}>
                 <Text style={styleLoginLogout.tag}>{"\t\t"} Phone: </Text>
-                <Text style={styleLoginLogout.prop}>{user.telNr}</Text>
+                <Text style={styleLoginLogout.prop}>{phone}</Text>
             </View>
 
             <View style = {styleLoginLogout.viewLine}/>
 
             <View style={styleLoginLogout.infoLine}>
-                <Text style={styleLoginLogout.tag}>{"\t\t"} Role: </Text>
-                <Text style={styleLoginLogout.prop}>{user.roles}</Text>
+                <Text style={styleLoginLogout.tag}>{"\t\t"} Roles: </Text>
+                <Text style={styleLoginLogout.prop}>{roles.length}</Text>
             </View>
 
             <View style = {styleLoginLogout.viewLine}/>
 
-            {/*<View>*/}
-            {/*    <Text>Amount of Favorites: </Text> <Text>{user.preferredSubjects.size}</Text>*/}
-            {/*</View>*/}
+            <View style={styleLoginLogout.infoLine}>
+                <Text style={styleLoginLogout.tag}>{"\t\t"} Favorites: </Text>
+                <Text style={styleLoginLogout.prop}>{favoriteSubjects.length}</Text>
+            </View>
+
+            <View style = {styleLoginLogout.viewLine}/>
+
+            <View style={styleLoginLogout.infoLine}>
+                <Text style={styleLoginLogout.tag}>{"\t\t"} TargetAud.: </Text>
+                <Text style={styleLoginLogout.prop}>{targetAudience}</Text>
+            </View>
 
             <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={logOut}>
                 <Text style={styleLoginLogout.loginText}>LOGOUT</Text>
