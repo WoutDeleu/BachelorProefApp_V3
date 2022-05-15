8b14f9c9f89a8e01c2dce35a31f7eb34931fd7db
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-05-15 20:39:30 +0200

    Small bugs

--- app/screens/ActionScreen/ActionScreen.js
+++ app/screens/ActionScreen/ActionScreen.js
@@ -3,21 +3,36 @@ import styleSubjectList from "../../styles/styleSubjectList";
 import {StatusBar} from "expo-status-bar";
 import styleActions from "../../styles/styleActions";
 import styleLoginLogout from "../../styles/styleLoginLogout";
+import isRole from "../../functions/isRole";
+import {useContext} from "react";
+import {AuthContext} from "../../Authentication/AuthProvider";
 
 function  ActionScreen({navigation}) {
-    return(
-        <View style={styleActions.container}>
-            <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={() => navigation.navigate('Users')}>
-                <Text style={styleLoginLogout.loginText}> USERS </Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styleLoginLogout.loginBtn}  onPress={() => navigation.navigate('Subjects')}>
-                <Text style={styleLoginLogout.loginText}> SUBJECTS </Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styleLoginLogout.loginBtn}  onPress={() => navigation.navigate('Companies')}>
-                <Text style={styleLoginLogout.loginText}> COMPANIES </Text>
-            </TouchableOpacity>
-        </View>
-    )
+    const {userInfo} = useContext(AuthContext)
+    if(isRole("ROLE_ADMIN", userInfo) || isRole("ROLE_COORDINATOR", userInfo) || isRole("ROLE_PROMOTOR", userInfo)) {
+        return (
+            <View style={styleActions.container}>
+                <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={() => navigation.navigate('Users')}>
+                    <Text style={styleLoginLogout.loginText}> USERS </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={() => navigation.navigate('Subjects')}>
+                    <Text style={styleLoginLogout.loginText}> SUBJECTS </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={() => navigation.navigate('Companies')}>
+                    <Text style={styleLoginLogout.loginText}> COMPANIES </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+    else {
+        return (
+            <View style={styleActions.container}>
+                <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={() => navigation.navigate('Companies')}>
+                    <Text style={styleLoginLogout.loginText}> COMPANIES </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
 
 export default ActionScreen;
--- app/screens/ActionScreen/Companies/CompaniesScreen.js
+++ app/screens/ActionScreen/Companies/CompaniesScreen.js
@@ -1,19 +1,33 @@
 import {View, Text, TouchableOpacity} from "react-native";
 import styleActions from "../../../styles/styleActions";
-import React from "react";
+import React, {useContext} from "react";
 import styleLoginLogout from "../../../styles/styleLoginLogout";
+import {AuthContext} from "../../../Authentication/AuthProvider";
+import isRole from "../../../functions/isRole";
 
 function CompanyScreen({navigation}) {
-    return(
-        <View style={styleActions.container}>
-            <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={() => navigation.navigate('comp_ApprComp')}>
-                <Text style={styleLoginLogout.loginText}> APPROVED COMPANIES </Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={() => navigation.navigate('comp_NonApprComp')}>
-                <Text style={styleLoginLogout.loginText}> NON APPROVED COMPANIES </Text>
-            </TouchableOpacity>
-        </View>
-    )
+    const {userInfo} = useContext(AuthContext)
+    if(!isRole("ROLE_STUDENT", userInfo)) {
+        return(
+            <View style={styleActions.container}>
+                <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={() => navigation.navigate('comp_ApprComp')}>
+                    <Text style={styleLoginLogout.loginText}> APPROVED COMPANIES </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={() => navigation.navigate('comp_NonApprComp')}>
+                    <Text style={styleLoginLogout.loginText}> NON APPROVED COMPANIES </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+    else {
+        return(
+            <View style={styleActions.container}>
+                <TouchableOpacity style={styleLoginLogout.loginBtn} onPress={() => navigation.navigate('comp_ApprComp')}>
+                    <Text style={styleLoginLogout.loginText}> APPROVED COMPANIES </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
 
 export default CompanyScreen;
